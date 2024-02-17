186cea9949b8ce7789b95eaa1e8c72050d854a59
Araceli-gp <araceliguzman0620@gmail.com>
2024-02-16 18:50:34 -0600

    first commit

---
+++ firebase-auth-main/assets/js/firebase/firebase.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+import {
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+    onSnapshot,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLwtbEU8DBVFjoT11xm2BWuUDZT0-l71s",
+  authDomain: "prueba-firebase-2504.firebaseapp.com",
+  projectId: "prueba-firebase-2504",
+  storageBucket: "prueba-firebase-2504.appspot.com",
+  messagingSenderId: "934737207752",
+  appId: "1:934737207752:web:1831520c849e48aca3a0fd",
+  measurementId: "G-MGEHHWMNN5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore();
+
+// Funciones del CRUD
+export const createTask = (title, description) => addDoc(collection(db, "tasks"), {title, description});
+
+export const getTask = id => getDoc(doc(db, "tasks", id));
+
+export const onGetTask = callback => onSnapshot(collection(db, "tasks"), callback);
+
+export const updateTask = (id, newFields) => updateDoc(doc(db, "tasks", id), newFields);
+
+export const deleteTask = id => deleteDoc(doc(db, "tasks", id));
